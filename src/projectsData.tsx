21d76cb5e4425d290be981364c7776f921e052c7
Iain <iain.hoolahan@gmail.com>
2023-08-30 21:23:46 +0100

    added repo links and updated description

--- src/projectsData.tsx
+++ src/projectsData.tsx
@@ -9,6 +9,8 @@ export interface Project {
   logo: string;
   description: string;
   url: string;
+  client_repo: string;
+  server_repo: string;
   technologies: string[];
 }
 
@@ -16,8 +18,10 @@ const projects: Project[] = [
   {
     title: "Tipsy Tourist",
     logo: tipsyTouristLogo,
-    description: "A travel app for tourists.",
+    description: "A web page to generate a pub crawl/tourist attractions itinerary",
     url: "http://www.tipsytourist.org/",
+    client_repo: "https://github.com/HOOLAHAN/tipsy-tourist",
+    server_repo: "https://github.com/HOOLAHAN/tipsy-tourist-lambda",
     technologies: [
       "JavaScript", "Node", "Express", "React", "Chakra UI", "Google API", "AWS S3", "AWS Lambda"
     ]
@@ -25,8 +29,10 @@ const projects: Project[] = [
   {
     title: "StratoStruct",
     logo: stratoStructLogo,
-    description: "A tool for finding pre-cast suppliers",
+    description: "A web page to find local suppliers of prefabricated construction products ranked by proximity",
     url: "http://www.stratostruct.com",
+    client_repo: "https://github.com/HOOLAHAN/stratostruct",
+    server_repo: "https://github.com/HOOLAHAN/stratostruct_server",
     technologies: [
       "MongoDB", "Express", "React", "Node", "HTML", "CSS", "Mapbox API", "AWS S3", "AWS EC2"
     ]
@@ -34,8 +40,10 @@ const projects: Project[] = [
   {
     title: "Find A Film",
     logo: findAFilmLogo,
-    description: "A tool for finding films to watch",
+    description: "A web page to search for films to watch",
     url: "http://findafilm.s3-website.eu-west-2.amazonaws.com/",
+    client_repo: "https://github.com/HOOLAHAN/Find_A_Film",
+    server_repo: "N/A",
     technologies: [
       "TypeScript", "Node", "Express", "React", "HTML", "CSS", "TMDB API", "AWS S3"
     ]
@@ -43,8 +51,10 @@ const projects: Project[] = [
   {
     title: "Harbour Contractors Ltd",
     logo: harbourContractors,
-    description: "A company website showcasing projects",
+    description: "A company website showcasing projects and services",
     url: "http://www.harbourcontractors.co.uk/",
+    client_repo: "https://github.com/HOOLAHAN/harbour_contractors_client",
+    server_repo: "https://github.com/HOOLAHAN/harbour_contractors_lambda",
     technologies: [
       "React", "Node", "Chakra UI", "Twilio API", "AWS S3", "AWS Lambda"
     ]
