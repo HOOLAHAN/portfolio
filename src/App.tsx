63ed871ca35f2fb8b357ebdf05a0c01318b8f020
Iain <iain.hoolahan@gmail.com>
2023-09-03 22:28:15 +0100

    implement document title change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 // src/App.tsx
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react'; // Import useEffect
 import ProjectCard from './ProjectCard';
 import projects from './projectsData';
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    document.title = 'Iain Hoolahan';
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
   return (
     <div className="App">
       <h1>My Portfolio</h1>
