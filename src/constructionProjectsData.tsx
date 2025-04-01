8a82a93f934ac8f0f581500062717fa24b6e3a81
Iain <iain.hoolahan@gmail.com>
2025-04-01 13:58:13 +0100

    refine project details wording

--- src/constructionProjectsData.tsx
+++ src/constructionProjectsData.tsx
@@ -22,7 +22,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "April 2023",
         endDate: "November 2023",
         location: "Gatwick, London",
-        details: "I carried out essential site engineering/surveying tasks for Lindner Prater Ltd, focusing on the precise installation of roofing, internal/external cladding and escalator glasing. This project required a high level of accuracy, and I conducted surveys to identify potential tolerance issues and interface challenges."
+        details: "I led essential site engineering and surveying tasks for Lindner Prater Ltd, focusing on the precise installation of roofing, internal/external cladding, and escalator glazing. My role required a high level of accuracy, where I conducted comprehensive surveys to identify potential tolerance issues and interface challenges, ensuring high-quality construction standards."
       },
       {
         projectName: "53 Clarendon Road",
@@ -30,7 +30,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "November 2023",
         endDate: "February 2024",
         location: "Watford, London",
-        details: "I worked on Regal's The Clarendon project in Watford, a high-rise residential development supported by PF Geomatics. I collaborated closely with the Regal team, overseeing the concrete subcontractor's work. The Clarendon features 168 private residences and amenities like a sky garden, concierge, cinema, and gym, playing a key role in Watford's regeneration. My responsibilities included quality checks, rebar inspections, and conducting pre-pour and post-pour surveys to maintain the tight tolerances needed for cladding. I also carried out general surveying and setting out verifications to ensure construction met the required quality standards."
+        details: "I played a key role in Regal's The Clarendon project, a high-rise residential development in Watford. Collaborating closely with the Regal team, I oversaw the concrete subcontractor's work, ensuring quality standards through meticulous quality checks, rebar inspections, and both pre-pour and post-pour surveys. I also conducted setting out verifications to maintain the stringent tolerances needed for cladding, contributing to the project's success in Watford's regeneration."
       },
       {
         projectName: "105 Victoria Street",
@@ -38,7 +38,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "June 2024",
         endDate: "April 2025",
         location: "Westminster, London",
-        details: "At 105 Victoria Street, I worked with Severfield, a leading steel specialist. My role involved performing site engineering duties to support steel erection and the installation of precast planks. I was responsible for surveying embedments cast into the core, producing fin plate setting out drawings, checking steel alignments, and communicating tirfor/pack requirements as needed. Additionally, I set out precast planks as they were landed on the steel structure and provided as-built survey reports of the completed works for handover."
+        details: "At 105 Victoria Street, I collaborated with Severfield, a leading steel specialist, to facilitate steel erection and the installation of precast planks. My responsibilities included surveying embedments cast into the core, creating fin plate setting out drawings, and verifying steel alignments. I effectively communicated tirfor/pack requirements, set out precast planks, and delivered as-built survey reports for handover, ensuring precision and quality."
       }
     ]
   },
@@ -52,7 +52,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "September 2020",
         endDate: "September 2022",
         location: "West Sussex, UK",
-        details: "Trippets was a new build project for a private client, which I project managed from demolition to completion through specialised subcontract packages. Key highlights of the project included piled foundations, a green oak frame, timber framing with high-quality insulation to achieve an EPC A+ rating, underfloor heating, a swimming pool, garage, pool house, landscaped gardens, solar panels, and a ground source heat pump utilising a network of 9 boreholes. The property also featured premium finishes, cedar cladding, a slate roof, and limestone walls."
+        details: "I managed the Trippets project, a £2M new build for a private client, from demolition to completion. The project included piled foundations, a green oak frame, timber framing with high-quality insulation to achieve an EPC A rating, underfloor heating, a swimming pool, garage, pool house, landscaped gardens, solar panels, and a ground source heat pump using 9 boreholes. The property featured premium finishes, cedar cladding, a slate roof, and limestone walls, reflecting my commitment to delivering high-quality results."
       },
       {
         projectName: "Java Sound",
@@ -60,7 +60,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "November 2021",
         endDate: "September 2022",
         location: "West Sussex, UK",
-        details: "Java Sound was a house extension designed to integrate modern features with traditional aesthetics. The extension included a green oak structure, bifolding doors, a spacious decking area, an air source heat pump, underfloor heating, larch cladding, and a zinc roof. I effectively managed project timelines and budgets while coordinating with subcontractors and keeping clients regularly informed of progress."
+        details: "I managed the Java Sound project, a £1M house extension that seamlessly blended modern features with traditional aesthetics. Key elements included a green oak structure, bifolding doors, a spacious decking area, an air source heat pump, underfloor heating, larch cladding, and a zinc roof. I coordinated subcontractors effectively and ensured regular client updates, maintaining project timelines and budget."
       }
     ]
   },
@@ -74,7 +74,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "April 2019",
         endDate: "September 2020",
         location: "Somerset, UK",
-        details: "As a Setting Out Engineer / Section Engineer at Hinkley Point C Nuclear Power Station, I gained invaluable field experience at Nuclear Island Unit 1, setting out and conducting QA checks for a record-breaking UK raft slab pour of 9000m3. As a Section Engineer, I ensured on-time material deliveries, planned, checked, and approved temporary works, conducted quality checks, managed Health & Safety through RAMS/briefings, resolved technical issues via RFIs/NCRs, and liaised with construction managers to discuss future project requirements. I also enhanced team performance and project delivery by mentoring junior engineers and maintaining robust client relationships."
+        details: "As a Setting Out Engineer / Section Engineer at Hinkley Point C, I gained invaluable field experience working on the Nuclear Island Unit 1, including a record-breaking UK raft slab pour of 9000m3. I managed material deliveries, quality checks, Health & Safety protocols, and technical issue resolution, while mentoring junior engineers and maintaining client relationships."
       },
       {
         projectName: "Crossrail: Liverpool Street Station",
@@ -82,7 +82,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "September 2016",
         endDate: "April 2019",
         location: "Liverpool Street, London",
-        details: "As a Package Manager at Crossrail Liverpool Street Station, I managed fit-out package subcontractors including back-of-house handrails, edge protection, ladders, access hatches, fall restraint systems, and front-of-house handrails and balustrades. I demonstrated leadership by coordinating subcontractor efforts, ensuring the timely and quality delivery of critical station infrastructure. I maintained strict adherence to Health & Safety and Quality Assurance protocols, managing comprehensive documentation including RAMS, ITPs, and QA packs. I also worked closely with commercial and planning teams to ensure all contract deliverables aligned with project timelines."
+        details: "As a Package Manager, I led the coordination of fit-out package subcontractors, including handrails, edge protection, and balustrades. I ensured adherence to Health & Safety and Quality Assurance standards while managing contract deliverables and collaborating with commercial and planning teams to meet project timelines."
       },
       {
         projectName: "Group Technical Services",
@@ -90,7 +90,7 @@ const constructionProjects: ConstructionProject[] = [
         startDate: "September 2015",
         endDate: "September 2016",
         location: "Dartford, UK",
-        details: "As a Temporary Works Design Engineer at Group Technical Services, I created geotechnical temporary works design solutions to enhance the infrastructure and safety of construction sites across the UK. During this role, I also developed my public speaking and communication skills by delivering effective presentations."
+        details: "In my role as a Temporary Works Design Engineer, I developed geotechnical temporary works design solutions to enhance construction site safety and infrastructure. I also improved my communication and presentation skills through delivering technical presentations."
       }
     ]
   }
